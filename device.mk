7b2cbd478e94f28b2fa7e7ff355366dc5146f4f2
Pierre Imai <imaipi@google.com>
2016-03-11 15:17:11 +0900

    Remove references to deleted dhcpcd
    Change-Id: I5e214232d59bdaaa9268735e00bbc9f937bf7d12
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- device.mk
+++ device.mk
@@ -84,7 +84,6 @@ PRODUCT_COPY_FILES += \
         frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
         frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
         device/linaro/hikey/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
-        device/linaro/hikey/android_dhcpcd.conf:system/etc/dhcpcd/dhcpcd.conf \
         device/linaro/hikey/audio/audio_policy.conf:system/etc/audio_policy.conf
 
 # Copy media codecs config file
