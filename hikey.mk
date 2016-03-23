6c65862a66c015d0d10d02bb5de972d5ddff1217
Dmitry Shmidt <dimitrysh@google.com>
2016-03-23 16:29:35 -0700

    hikey: Move TARGET_KERNEL_USE_4_1 check to hikey.mk
    device.mk is loaded before BoadConfig.mk, therefore
    TARGET_KERNEL_USE_4_1 should be checked earlier.
    
    Change-Id: I1a273b07042fcd519e6d1b2c7a55ed4540341200
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- hikey.mk
+++ hikey.mk
@@ -1,3 +1,9 @@
+ifeq ($(TARGET_KERNEL_USE_4_1), true)
+TARGET_PREBUILT_KERNEL := device/linaro/hikey-kernel/Image-4.1
+TARGET_PREBUILT_DTB := device/linaro/hikey-kernel/hi6220-hikey.dtb-4.1
+TARGET_FSTAB := fstab.hikey-4.1
+endif
+
 #
 # Inherit the full_base and device configurations
 $(call inherit-product, device/linaro/hikey/device.mk)
