40a042d1093df12b3b725a82118ba2dcec52f718
Steven Moreland <smoreland@google.com>
2016-10-14 15:57:35 +0000

    Revert "Add Vibrator default impl to device build"
    This reverts commit dbdaa429165e744772a67c54b0837607a7e9f166.
    
    Change-Id: I0e2c48c5aa1c93d07bec4143fd71c75675152977

--- device.mk
+++ device.mk
@@ -78,10 +78,6 @@ PRODUCT_PACKAGES +=	TIInit_11.8.32.bts \
 # PowerHAL
 PRODUCT_PACKAGES += power.hikey
 
-# VibratorHAL
-PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl
-
 # Copy hardware config file(s)
 PRODUCT_COPY_FILES +=  \
         device/linaro/hikey/etc/permissions/android.hardware.screen.xml:system/etc/permissions/android.hardware.screen.xml \
