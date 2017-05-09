fd855198890683d3b4981ffb4777e8ece00ec460
Roshan Pius <rpius@google.com>
2017-05-09 13:13:36 -0700

    hikey: Stop running vendor HAL daemon
    This is anyway non-functional on this platform.
    
    Bug: 36886769
    Test: Compiles
    Change-Id: I0bdd00f01c2b2d3373e7d3cbf4c9a8fc9e4118ab

--- device-common.mk
+++ device-common.mk
@@ -25,7 +25,6 @@ PRODUCT_PACKAGES += ssh sftp scp sshd ssh-keygen sshd_config start-ssh uim
 
 # Add wifi-related packages
 PRODUCT_PACKAGES += libwpa_client wpa_supplicant hostapd wificond wifilogd
-PRODUCT_PACKAGES += android.hardware.wifi@1.0-service
 PRODUCT_PROPERTY_OVERRIDES += wifi.interface=wlan0 \
                               wifi.supplicant_scan_interval=15
 
