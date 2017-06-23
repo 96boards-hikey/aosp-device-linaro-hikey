f2de134804b629eba55f77d3e99daec99350734c
Jake Weinstein <xboxlover360@gmail.com>
2017-06-23 12:23:08 -0400

    hikey: Optimize CPU Targets for each variant
    hikey has A53 cores, while hikey960 has A73 and A53 cores.  Bionic
    now has A73-specific configuration, so this CL uses the optimal
    CPU configuration for both targets.
    
    Change-Id: I5f15c8fdc43fcab5e4f01b3685f3e19b9916c0bf

--- hikey/BoardConfig.mk
+++ hikey/BoardConfig.mk
@@ -1,5 +1,8 @@
 include device/linaro/hikey/BoardConfigCommon.mk
 
+TARGET_CPU_VARIANT := cortex-a53
+TARGET_2ND_CPU_VARIANT := cortex-a53
+
 TARGET_BOARD_PLATFORM := hikey
 ifeq ($(TARGET_KERNEL_USE), 3.18)
 BOARD_KERNEL_CMDLINE := console=ttyAMA3,115200 androidboot.console=ttyAMA3 androidboot.hardware=hikey firmware_class.path=/system/etc/firmware efi=noruntime
