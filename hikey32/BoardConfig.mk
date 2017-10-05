5f59b7a9d702bc740dc2cd04776a4d169d391cec
Dmitry Shmidt <dimitrysh@google.com>
2017-10-05 16:05:45 -0700

    hikey: Add hikey32 target for arm32
    Change-Id: I3db05ce6678bd50efb4839f1eca634cccf0b8891
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

---
+++ hikey32/BoardConfig.mk
@@ -0,0 +1,18 @@
+include device/linaro/hikey/hikey/BoardConfig.mk
+
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := generic
+
+TARGET_2ND_ARCH :=
+TARGET_2ND_ARCH_VARIANT :=
+TARGET_2ND_CPU_ABI :=
+TARGET_2ND_CPU_ABI2 :=
+TARGET_2ND_CPU_VARIANT :=
+
+TARGET_SUPPORTS_64_BIT_APPS := false
+
+
+
