2b61444e954847fda9f3f1d64fb9476fbfe1d85d
Chia-I Wu <olv@google.com>
2017-10-19 15:07:35 -0700

    hikey: enable HIDL HWC
    This will use FB->HWC2 adapter.
    
    Test: boots hikey and hikey960
    Change-Id: I70995d71f70c3cf6bd3bbaca8756ee0a6644b32a

--- device-common.mk
+++ device-common.mk
@@ -58,6 +58,7 @@ PRODUCT_PACKAGES += libGLES_android
 # Graphics HAL
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.mapper@2.0-impl
 
 # Memtrack
