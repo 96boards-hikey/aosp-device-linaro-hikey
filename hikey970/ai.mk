32f919339b20ed1fd831aafa12a9c569d9c8076c
Guodong Xu <guodong.xu@linaro.org>
2018-03-16 09:21:42 +0800

    ai: update ai config files for removed models
    Removed models in previous commit. This commit update the config to
    reflect that.
    
    Signed-off-by: SiWei Xu <xusiwei1@hisilicon.com>
    Signed-off-by: Guodong Xu <guodong.xu@linaro.org>

--- hikey970/ai.mk
+++ hikey970/ai.mk
@@ -46,58 +46,6 @@ PRODUCT_COPY_FILES += \
 	$(models_dir)/ImageClassification/caffe/build/IC_conv_112x112.cambricon:$(vendor_dir)/etc/models/ImageClassification/caffe/build/IC_conv_112x112.cambricon \
 	$(models_dir)/ImageClassification/caffe/build/IC_conv_112x112_mean:$(vendor_dir)/etc/models/ImageClassification/caffe/build/IC_conv_112x112_mean \
 	$(models_dir)/ImageClassification/caffe/build/IC_conv_112x112_synset_words.txt:$(vendor_dir)/etc/models/ImageClassification/caffe/build/IC_conv_112x112_synset_words.txt \
-	$(models_dir)/ImageSegmentation/caffe/build/IS_conv_473x473.cambricon:$(vendor_dir)/etc/models/ImageSegmentation/caffe/build/IS_conv_473x473.cambricon \
-	$(models_dir)/ImageSegmentation/caffe/build/IS_conv_473x473_mean:$(vendor_dir)/etc/models/ImageSegmentation/caffe/build/IS_conv_473x473_mean \
-	$(models_dir)/CameraDlsr/caffe/build/HWSR-s_506_506.cambricon:$(vendor_dir)/etc/models/CameraDlsr/caffe/build/HWSR-s_506_506.cambricon \
-	$(models_dir)/CameraDlsr/caffe/build/HWSR-s_666_378.cambricon:$(vendor_dir)/etc/models/CameraDlsr/caffe/build/HWSR-s_666_378.cambricon \
-	$(models_dir)/CameraDlsr/caffe/build/HWSR-s_666_506.cambricon:$(vendor_dir)/etc/models/CameraDlsr/caffe/build/HWSR-s_666_506.cambricon \
-	$(models_dir)/CameraDlsr/caffe/build/HWSR-s_666_346.cambricon:$(vendor_dir)/etc/models/CameraDlsr/caffe/build/HWSR-s_666_346.cambricon \
-	$(models_dir)/CameraDlsr/caffe/build/HWSR-s_586_586.cambricon:$(vendor_dir)/etc/models/CameraDlsr/caffe/build/HWSR-s_586_586.cambricon \
-	$(models_dir)/CameraDlsr/caffe/build/HWSR-s_778_394.cambricon:$(vendor_dir)/etc/models/CameraDlsr/caffe/build/HWSR-s_778_394.cambricon \
-	$(models_dir)/CameraDlsr/caffe/build/HWSR-s_778_586.cambricon:$(vendor_dir)/etc/models/CameraDlsr/caffe/build/HWSR-s_778_586.cambricon \
-	$(models_dir)/ObjectDetection/caffe/build/od.cambricon:$(vendor_dir)/etc/models/ObjectDetection/caffe/build/od.cambricon \
-	$(models_dir)/SuperResolution/caffe/build/SR_face_x1/SR_face_x1_112x112.cambricon:$(vendor_dir)/etc/models/SuperResolution/caffe/build/SR_face_x1_112x112.cambricon  \
-	$(models_dir)/SuperResolution/caffe/build/SR_face_x1/SR_face_x1_312x412.cambricon:$(vendor_dir)/etc/models/SuperResolution/caffe/build/SR_face_x1_312x412.cambricon  \
-	$(models_dir)/SuperResolution/caffe/build/SR_face_x1/SR_face_x1_372x492.cambricon:$(vendor_dir)/etc/models/SuperResolution/caffe/build/SR_face_x1_372x492.cambricon  \
-	$(models_dir)/SuperResolution/caffe/build/SR_face_x1/SR_face_x1_412x312.cambricon:$(vendor_dir)/etc/models/SuperResolution/caffe/build/SR_face_x1_412x312.cambricon  \
-	$(models_dir)/SuperResolution/caffe/build/SR_face_x1/SR_face_x1_412x545.cambricon:$(vendor_dir)/etc/models/SuperResolution/caffe/build/SR_face_x1_412x545.cambricon  \
-	$(models_dir)/SuperResolution/caffe/build/SR_face_x1/SR_face_x1_492x372.cambricon:$(vendor_dir)/etc/models/SuperResolution/caffe/build/SR_face_x1_492x372.cambricon  \
-	$(models_dir)/SuperResolution/caffe/build/SR_face_x1/SR_face_x1_492x652.cambricon:$(vendor_dir)/etc/models/SuperResolution/caffe/build/SR_face_x1_492x652.cambricon  \
-	$(models_dir)/SuperResolution/caffe/build/SR_face_x1/SR_face_x1_530x530.cambricon:$(vendor_dir)/etc/models/SuperResolution/caffe/build/SR_face_x1_530x530.cambricon  \
-	$(models_dir)/SuperResolution/caffe/build/SR_face_x1/SR_face_x1_545x412.cambricon:$(vendor_dir)/etc/models/SuperResolution/caffe/build/SR_face_x1_545x412.cambricon  \
-	$(models_dir)/SuperResolution/caffe/build/SR_face_x1/SR_face_x1_652x492.cambricon:$(vendor_dir)/etc/models/SuperResolution/caffe/build/SR_face_x1_652x492.cambricon  \
-	$(models_dir)/SuperResolution/caffe/build/SR_face_x3/SR_face_x3_112x112.cambricon:$(vendor_dir)/etc/models/SuperResolution/caffe/build/SR_face_x3_112x112.cambricon  \
-	$(models_dir)/SuperResolution/caffe/build/SR_face_x3/SR_face_x3_312x412.cambricon:$(vendor_dir)/etc/models/SuperResolution/caffe/build/SR_face_x3_312x412.cambricon  \
-	$(models_dir)/SuperResolution/caffe/build/SR_face_x3/SR_face_x3_372x492.cambricon:$(vendor_dir)/etc/models/SuperResolution/caffe/build/SR_face_x3_372x492.cambricon  \
-	$(models_dir)/SuperResolution/caffe/build/SR_face_x3/SR_face_x3_412x312.cambricon:$(vendor_dir)/etc/models/SuperResolution/caffe/build/SR_face_x3_412x312.cambricon  \
-	$(models_dir)/SuperResolution/caffe/build/SR_face_x3/SR_face_x3_412x545.cambricon:$(vendor_dir)/etc/models/SuperResolution/caffe/build/SR_face_x3_412x545.cambricon  \
-	$(models_dir)/SuperResolution/caffe/build/SR_face_x3/SR_face_x3_492x372.cambricon:$(vendor_dir)/etc/models/SuperResolution/caffe/build/SR_face_x3_492x372.cambricon  \
-	$(models_dir)/SuperResolution/caffe/build/SR_face_x3/SR_face_x3_492x652.cambricon:$(vendor_dir)/etc/models/SuperResolution/caffe/build/SR_face_x3_492x652.cambricon  \
-	$(models_dir)/SuperResolution/caffe/build/SR_face_x3/SR_face_x3_530x530.cambricon:$(vendor_dir)/etc/models/SuperResolution/caffe/build/SR_face_x3_530x530.cambricon  \
-	$(models_dir)/SuperResolution/caffe/build/SR_face_x3/SR_face_x3_545x412.cambricon:$(vendor_dir)/etc/models/SuperResolution/caffe/build/SR_face_x3_545x412.cambricon  \
-	$(models_dir)/SuperResolution/caffe/build/SR_face_x3/SR_face_x3_652x492.cambricon:$(vendor_dir)/etc/models/SuperResolution/caffe/build/SR_face_x3_652x492.cambricon  \
-	$(models_dir)/SuperResolution/caffe/build/SR_common_x1/SR_x1_112x112.cambricon:$(vendor_dir)/etc/models/SuperResolution/caffe/build/SR_x1_112x112.cambricon  \
-	$(models_dir)/SuperResolution/caffe/build/SR_common_x1/SR_x1_312x412.cambricon:$(vendor_dir)/etc/models/SuperResolution/caffe/build/SR_x1_312x412.cambricon  \
-	$(models_dir)/SuperResolution/caffe/build/SR_common_x1/SR_x1_372x492.cambricon:$(vendor_dir)/etc/models/SuperResolution/caffe/build/SR_x1_372x492.cambricon  \
-	$(models_dir)/SuperResolution/caffe/build/SR_common_x1/SR_x1_412x312.cambricon:$(vendor_dir)/etc/models/SuperResolution/caffe/build/SR_x1_412x312.cambricon  \
-	$(models_dir)/SuperResolution/caffe/build/SR_common_x1/SR_x1_412x545.cambricon:$(vendor_dir)/etc/models/SuperResolution/caffe/build/SR_x1_412x545.cambricon  \
-	$(models_dir)/SuperResolution/caffe/build/SR_common_x1/SR_x1_492x372.cambricon:$(vendor_dir)/etc/models/SuperResolution/caffe/build/SR_x1_492x372.cambricon  \
-	$(models_dir)/SuperResolution/caffe/build/SR_common_x1/SR_x1_492x652.cambricon:$(vendor_dir)/etc/models/SuperResolution/caffe/build/SR_x1_492x652.cambricon  \
-	$(models_dir)/SuperResolution/caffe/build/SR_common_x1/SR_x1_530x530.cambricon:$(vendor_dir)/etc/models/SuperResolution/caffe/build/SR_x1_530x530.cambricon  \
-	$(models_dir)/SuperResolution/caffe/build/SR_common_x1/SR_x1_545x412.cambricon:$(vendor_dir)/etc/models/SuperResolution/caffe/build/SR_x1_545x412.cambricon  \
-	$(models_dir)/SuperResolution/caffe/build/SR_common_x1/SR_x1_652x492.cambricon:$(vendor_dir)/etc/models/SuperResolution/caffe/build/SR_x1_652x492.cambricon  \
-	$(models_dir)/SuperResolution/caffe/build/SR_common_x3/SR_x3_112x112.cambricon:$(vendor_dir)/etc/models/SuperResolution/caffe/build/SR_x3_112x112.cambricon  \
-	$(models_dir)/SuperResolution/caffe/build/SR_common_x3/SR_x3_312x412.cambricon:$(vendor_dir)/etc/models/SuperResolution/caffe/build/SR_x3_312x412.cambricon  \
-	$(models_dir)/SuperResolution/caffe/build/SR_common_x3/SR_x3_372x492.cambricon:$(vendor_dir)/etc/models/SuperResolution/caffe/build/SR_x3_372x492.cambricon  \
-	$(models_dir)/SuperResolution/caffe/build/SR_common_x3/SR_x3_412x312.cambricon:$(vendor_dir)/etc/models/SuperResolution/caffe/build/SR_x3_412x312.cambricon  \
-	$(models_dir)/SuperResolution/caffe/build/SR_common_x3/SR_x3_412x545.cambricon:$(vendor_dir)/etc/models/SuperResolution/caffe/build/SR_x3_412x545.cambricon  \
-	$(models_dir)/SuperResolution/caffe/build/SR_common_x3/SR_x3_492x372.cambricon:$(vendor_dir)/etc/models/SuperResolution/caffe/build/SR_x3_492x372.cambricon  \
-	$(models_dir)/SuperResolution/caffe/build/SR_common_x3/SR_x3_492x652.cambricon:$(vendor_dir)/etc/models/SuperResolution/caffe/build/SR_x3_492x652.cambricon  \
-	$(models_dir)/SuperResolution/caffe/build/SR_common_x3/SR_x3_530x530.cambricon:$(vendor_dir)/etc/models/SuperResolution/caffe/build/SR_x3_530x530.cambricon  \
-	$(models_dir)/SuperResolution/caffe/build/SR_common_x3/SR_x3_545x412.cambricon:$(vendor_dir)/etc/models/SuperResolution/caffe/build/SR_x3_545x412.cambricon  \
-	$(models_dir)/SuperResolution/caffe/build/SR_common_x3/SR_x3_652x492.cambricon:$(vendor_dir)/etc/models/SuperResolution/caffe/build/SR_x3_652x492.cambricon  \
-	$(models_dir)/SuperResolution/caffe/build/SR_PD_112x112/SR_PD_112x112.cambricon:$(vendor_dir)/etc/models/SuperResolution/caffe/build/SR_PD_112x112.cambricon  \
-	$(models_dir)/SuperResolution/caffe/build/SR_PD_112x112/SR_PD_112x112_mean:$(vendor_dir)/etc/models/SuperResolution/caffe/build/SR_PD_112x112_mean
 
 endif
 
