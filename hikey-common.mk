85e4f25b91f611a97097d3e3489c18ac904bed7d
Steven Moreland <smoreland@google.com>
2018-03-19 14:00:33 -0700

    ENFORCE_VINTF_MANIFEST on hikey*
    When this is not enforced (required for devices launched >= O,
    so it makes sense on this line of reference devices), there can
    be a race condition where if a service is hwbinder (not passthrough)
    and it starts up late, the system will assume it is missing.
    
    Test: boot hikey960 to start, no crashes, lshal
    Bug: 67974785
    Merged-In: If14c758bf59109340b1744feecdc9c05bde11dfc
    Change-Id: If14c758bf59109340b1744feecdc9c05bde11dfc

--- hikey-common.mk
+++ hikey-common.mk
@@ -15,6 +15,8 @@ endif
 
 TARGET_PREBUILT_DTB := device/linaro/hikey-kernel/hi6220-hikey.dtb-$(TARGET_KERNEL_USE)
 
+PRODUCT_ENFORCE_VINTF_MANIFEST_OVERRIDE := true
+
 ifeq ($(TARGET_KERNEL_USE), 3.18)
   TARGET_FSTAB := fstab.hikey-$(TARGET_KERNEL_USE)
   HIKEY_USE_LEGACY_TI_BLUETOOTH := true
